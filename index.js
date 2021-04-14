08d4a171eedba249e90ede3739c680b8a898598d
Vijayesh Jeevan <vijayeshjeevan.cs19@bmsce.ac.in>
2021-04-14 16:37:05 +0530

    FINAL!!!

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 var fs = require("fs");
 const bodyParser = require("body-parser");
-const get_data = require("./list_activities4");
+const get_data = require("./list_activities");
 const lineReaderSync = require("line-reader-sync");
-const { sfa } = require("./list_activities4");
- 
+const { sfa } = require("./list_activities");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
@@ -19,16 +19,24 @@ app.get("/", (req, res) => {
 app.post("/read", (req, res) => {
   var a = req.body.student_email;
   //var d = get_data.lk(a)
- // d.then(function(value){
-    //console.log("This index.js ",value, d);
-  res.render("read", { 
-    
-    contents :  get_data.lk(a, get_data.sfa, get_data.dw),
+  // d.then(function(value){
+  //console.log("This index.js ",value, d);
+  var data = get_data.lk(a, get_data.sfa, get_data.dw);
+  //console.log(data);
+  var names = [];
+  var days = [];
+  for (var i = 1; i < data.length; i++) {
+    names.push(data[i][2]);
+    days.push(data[i][0]);
+  }
+  console.log(days, names);
+  res.render("read", {
+    total_days: data[0],
+    data: days,
+    lables: names,
   });
-  }); 
-
+});
 
 app.listen(3000, () => {
   console.log("Server listening to port: 3000");
 });
- 
--- list_activities.js
+++ list_activities.js
@@ -1,78 +1,78 @@
-const lineReader = require('line-reader');
-const LineReaderSync = require("line-reader-sync")
+const lineReader = require("line-reader");
+const LineReaderSync = require("line-reader-sync");
 
-function link_student_id(email_ID, callback, superman){
-    console.log("Student Email ID: "+email_ID);
-    var lrs1=new LineReaderSync("student_id_email.csv");
-    while(true){
-        var line = lrs1.readline();
-        if(line === null){
-            break;
-        }
-        var email_ID_file=line.split(',')[1].trim();
-        var student_id=line.split(',')[0];
-        //console.log("***"+email_ID+"***"+line.split(',')[1].trim()+"***\n");
-        if(email_ID.trim() == email_ID_file ){
-            console.log("H1");
-            var d = callback(student_id, superman); 
-            //console.log(d);
-            console.log("SUCCESS!\n");
-            return d;
-            
-        }
+function link_student_id(email_ID, callback, superman) {
+  console.log("Student Email ID: " + email_ID);
+  var lrs1 = new LineReaderSync("student_id_email.csv");
+  while (true) {
+    var line = lrs1.readline();
+    if (line === null) {
+      break;
     }
+    var email_ID_file = line.split(",")[1].trim();
+    var student_id = line.split(",")[0];
+    //console.log("***"+email_ID+"***"+line.split(',')[1].trim()+"***\n");
+    if (email_ID.trim() == email_ID_file) {
+      console.log("H1");
+      var d = callback(student_id, superman);
+      //console.log(d);
+      console.log("SUCCESS!\n");
+      return d;
+    }
+  }
 }
 
-function days_worked(stu_timestamps_people_id,student_ID) {
-    var all_data = [];
-    var days=[];
-    var days_work = 0;
-    var stu_timestamp_people_id=stu_timestamps_people_id.split("*&*");
-    stu_timestamp_people_id.forEach(element => {
-        var stu_ID=element.split('/')[1];
-        var timestamp_single=element.split('/')[0];
-        if(stu_ID == student_ID) {
-            var date=timestamp_single.split('T')[0];
-            days.push(date);
-        }
-    });
-    var days_set=new Set(days);
-    days_work = days_set.size;
-    let days_set_array = [];
-    days_set.forEach(v => days_set_array.push(v));
-    console.log("ARRAY OF DAYS "+days_set_array);
-    console.log("No. of days ",days_work);
-    all_data.push(days_work);
-    all_data.push(days_set_array);
-    return all_data;
+function days_worked(stu_timestamps_people_id, student_ID) {
+  var all_data = [];
+  var days = [];
+  var days_work = 0;
+  var stu_timestamp_people_id = stu_timestamps_people_id.split("*&*");
+  stu_timestamp_people_id.forEach((element) => {
+    var stu_ID = element.split("/")[1];
+    var timestamp_single = element.split("/")[0];
+    if (stu_ID == student_ID) {
+      var date = timestamp_single.split("T")[0];
+      days.push(date);
+    }
+  });
+  var days_set = new Set(days);
+  days_work = days_set.size;
+  let days_set_array = [];
+  days_set.forEach((v) => days_set_array.push(v));
+  //console.log("ARRAY OF DAYS " + days_set_array);
+  //console.log("No. of days ", days_work);
+  all_data.push(days_work);
+  all_data.push(days_set_array);
+  return all_data;
 }
 
 function student_find_activities(student_ID, batman) {
-    var total_days=0;
-    var array = [];
-    var data_p_array = [];
-    var total_days_1 = [];
-    var lrs = new LineReaderSync("drive_activitiy_files.txt"); 
-    while(true){
-            var line = lrs.readline();
-            if(line === null){
-                break;
-            }
-            else{
-                var stu_doc_name = line.split('***')[2];
-                var stu_timestamps_people_id = line.split('***')[3];
-                if(line.includes(student_ID)) {
-                    //array.push(stu_doc_name)
-                    total_days_1= batman(stu_timestamps_people_id,student_ID);
-                    total_days = total_days + total_days_1[0];
-                    total_days_1.push(stu_doc_name);
-                    data_p_array.push(total_days_1);
-                }
-            }
-
+  var total_days = 0;
+  var data_p_array = [];
+  var total_days_1 = [];
+  var lrs = new LineReaderSync("drive_activitiy_files.txt");
+  while (true) {
+    var line = lrs.readline();
+    if (line === null) {
+      break;
+    } else {
+      var stu_doc_name = line.split("***")[2];
+      var stu_timestamps_people_id = line.split("***")[3];
+      if (line.includes(student_ID)) {
+        //array.push(stu_doc_name)
+        total_days_1 = batman(stu_timestamps_people_id, student_ID);
+        total_days = total_days + total_days_1[0];
+        total_days_1.push(stu_doc_name);
+        data_p_array.push(total_days_1);
+      }
     }
-    data_p_array.unshift(total_days);
-    console.log(data_p_array);
-    return data_p_array;
+  }
+  data_p_array.unshift(total_days);
+  //console.log(data_p_array);
+  return data_p_array;
 }
-module.exports = { lk:link_student_id, sfa : student_find_activities, dw : days_worked}; 
+module.exports = {
+  lk: link_student_id,
+  sfa: student_find_activities,
+  dw: days_worked,
+};
